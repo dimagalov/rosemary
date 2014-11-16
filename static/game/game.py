6d207fc56fec1c3ac8827994c5fd1feb3e6fab10
Maxim Grishkin <grishkin.max@gmail.com>
2014-11-16 14:52:50 +0300

    bitch please

---
+++ static/game/game.py
@@ -0,0 +1,81 @@
+from geom import *
+from snake import *
+import json
+from copy import copy
+
+from Pubnub import Pubnub
+
+PUBLISH_KEY = "pub-c-33787580-d63f-4c10-a274-4673c54b6655"
+SUBSCRIBE_KEY = "sub-c-79472c46-6cd4-11e4-ab04-02ee2ddab7fe"
+
+pubnub = Pubnub(PUBLISH_KEY, SUBSCRIBE_KEY, None, False)
+
+defaultParams = {
+    "rounds": 50,
+    "playersCount": 2,
+    "colors": ["#00f", "#f00","#006400", "#000", "orange", "#000"],
+    "ucolors": ["#88f", "#f88","#3cb371", "#666", "white"],
+    "canvasSizes": (640, 480),
+}
+
+class Game:
+    def __init__(self, _id, _players, _params = defaultParams):
+        self.id = _id
+        self.players = copy(_players)
+
+        self.rounds = _params["rounds"]
+        self.playersCount = _params["playersCount"]
+        self.colors = _params["colors"]
+        self.ucolors = _params["ucolors"]
+        self.canvasSizes = _params["canvasSizes"]
+
+        self.snakes = []
+        self.alive = []
+        self.aliveCount = self.playersCount
+        self.walls = [
+            Point(0, 0), 
+            Point(self.canvasSizes[0], 0), 
+            Point(self.canvasSizes[0], 
+            self.canvasSizes[1]), 
+            Point(0, self.canvasSizes[1])
+        ]
+        for i in range(self.playersCount):
+            self.snakes.append(Snake(self.canvasSizes, self.colors[i], self.ucolors[i], self.players[i]))
+            self.alive.append(True)
+
+    def check(self, s):
+        if self.snakes[s].chole >= -1:
+            return False
+        length = len(self.snakes[s].points)
+        ind = False
+        ind |= (self.snakes[s].position.x < 0)
+        ind |= (self.snakes[s].position.x > self.canvasSizes[0])
+        ind |= (self.snakes[s].position.y < 0)
+        ind |= (self.snakes[s].position.y > self.canvasSizes[1])
+        for i in range(self.playersCount):
+            d = 1
+            if i == s:
+                d += 2
+            for j in range(0, len(self.snakes[i].points) - d):
+                if not (isinstance(self.snakes[i].points[j], FakePoint) or isinstance(self.snakes[i].points[j + 1], FakePoint)):
+                    ind |= segmentsIntersect(self.snakes[s].points[length - 1], self.snakes[s].points[length - 2], self.snakes[i].points[j], self.snakes[i].points[j + 1])
+        return ind
+
+    def update(self, deltas):
+        toDraw = []
+        for i in range(self.playersCount):
+            if self.alive[i]:
+                toDraw.append(self.snakes[i].update(deltas[i]))
+        for i in range(self.playersCount):
+            if self.alive[i] and self.check(i):
+                self.alive[i] = False
+                self.aliveCount -= 1
+        pubnub.publish(str(self.id) + "_sis_draw", json.dumps(toDraw))
+
+    def restart(self):
+        self.snakes = []
+        self.alive = []
+        self.aliveCount = self.playersCount
+        for i in range(self.playersCount):
+            self.snakes.append(Snake(self.canvasSizes, self.colors[i], self.ucolors[i], self.players[i]))
+            self.alive.append(True)
---
+++ static/game_js/game/snake.js
@@ -0,0 +1,82 @@
+/**
+ * Declaration of Snake, the main structure we will operate with
+ */
+var Snake = function(_player, canvas, color, ucolor) {
+    this.player = _player
+
+    /* Canvas attributes */
+    this.canvas        = canvas;
+    this.canvas_width  = parseInt(canvas.prop("width"), 10);
+    this.canvas_height = parseInt(canvas.prop("height"), 10);
+
+    /* Initaial values of Snake's attributes */
+    this.color    = color;                                                // color of the snake
+    this.ucolor   = ucolor;                                               // color of snake, while it's digging a hole
+    this.position = new Point(randNumber(100, this.canvas_width - 100),
+                              randNumber(100, this.canvas_height - 100)); // initial position of the snake
+    this.angle    = Math.random() * Math.PI * 2;                          // initial direction of movement
+    this.points   = [this.position];                                      // array of all Snake's points
+
+    /*  Motion Constants  */
+    this.cangle       = 0.1;  // rotation angle
+    this.cspeed       = 3.5;  // motion speed
+    this.choleprob    = 0.08; // probability of new hole to appear
+    this.chole        = 0;    // length of the current hole (can be negative, if the Snake is tangible now)
+    this.choleborder  = -10;  // if this.chole < this.choleborder, then we can create new hole
+    this.choleminsize = 5;    // minimal hole size
+    this.cholemaxsize = 10;   // maximal hole size
+
+    /**
+    * Updates Snake's state according to the current movement direction
+    */
+    this.update = function (direction) {
+        // we don't won't to draw a hole by default
+        --this.chole;
+
+        // memorize the previous position
+        previous = this.position;
+
+        // calculate new angle according to current direction
+        this.angle += direction * this.cangle;
+
+        // angle should be in [0; 2 * pi] range
+        if (this.angle < 0)
+            this.angle += 2 * Math.PI;
+        if (this.angle >= 2 * Math.PI)
+            this.angle -= 2 * Math.PI;
+
+        // calculate new position
+        this.position = shiftPoint(this.position, rotateVector(new Vector(0, this.cspeed), this.angle));
+
+        // if we are in a hole now or we want to create one and random allows us to do so
+        if (this.chole > 0 || (this.chole < this.choleborder && Math.random() < this.choleprob)) {
+            // then we should add new fakePoint to our snake
+            this.points.push(new fakePoint());
+
+            // if we created a new hole, then we should choose it's length
+            if (this.chole < this.choleborder) {
+                this.chole = randNumber(this.choleminsize, this.cholemaxsize + 1);
+            }
+
+            // draw invisible the part of the Snake from previous position to current
+            this.canvas.drawLine({
+                strokeStyle: this.ucolor,                // the hole is invisible, so the colour is equal to the canvas colour
+                strokeWidth: 5,                          // width of the Snake
+                rounded: true,                           // our Snake should look pretty
+                x1: previous.x, y1: previous.y,          // one end of the segment
+                x2: this.position.x, y2: this.position.y // other end of the segment
+            });
+        }
+        else {
+            // draw visible the part of the Snake from previous position to current
+            this.points.push(this.position);
+            this.canvas.drawLine({
+                strokeStyle: this.color,                 // snake is visible, so the colour is equal to the Snake colour
+                strokeWidth: 5,                          // width of the Snake
+                rounded: true,                           // our Snake should look pretty
+                x1: previous.x, y1: previous.y,          // one end of the segment
+                x2: this.position.x, y2: this.position.y // other end of the segment
+            });
+        }
+    };
+};
