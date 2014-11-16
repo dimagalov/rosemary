a962aaa65ae1a94825e2269c1c49c92e25c687b9
Maxim Grishkin <grishkin.max@gmail.com>
2014-11-16 09:09:31 +0300

    SOOKA IT WORKS

--- game/snake.py
+++ game/snake.py
@@ -39,17 +39,17 @@ class Snake:
             toDraw = {
                 "strokeStyle": self.ucolor,
                 "strokeWidth": 5,
-                rounded: True,
-                x1: previous.x, y1: previous.y,
-                x2: self.position.x, y2: self.position.y
+                "rounded": True,
+                "x1": previous.x, "y1": previous.y,
+                "x2": self.position.x, "y2": self.position.y
             }
         else:
             self.points.append(self.position)
             toDraw = {
                 "strokeStyle": self.color,
                 "strokeWidth": 5,
-                rounded: True,
-                x1: previous.x, y1: previous.y,
-                x2: self.position.x, y2: self.position.y
+                "rounded": True,
+                "x1": previous.x, "y1": previous.y,
+                "x2": self.position.x, "y2": self.position.y
             }
         return toDraw
